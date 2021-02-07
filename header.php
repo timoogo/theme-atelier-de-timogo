b1ac00609ee4a3dec841b97febc7accf78accc61
Timothée Gaultier <timothee.gaultier@gmail.com>
2021-02-07 23:19:33 +0100

    :tada: init + simple header styles

---
+++ header.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * The header for our theme
+ *
+ * This is the template that displays all of the <head> section and everything up until <div id="content">
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package atelierdetimogo
+ */
+
+?>
+<!doctype html>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="profile" href="https://gmpg.org/xfn/11">
+
+	<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+<?php wp_body_open(); ?>
+	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'atelierdetimogo' ); ?></a>
+
+	<header class="site-header">
+		<div class="site-branding">
+			<?php
+			the_custom_logo();
+			if ( is_front_page() && is_home() ) :
+				?>
+				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+				<?php
+			else :
+				?>
+				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+				<?php
+			endif;
+			$atelierdetimogo_description = get_bloginfo( 'description', 'display' );
+			if ( $atelierdetimogo_description || is_customize_preview() ) :
+				?>
+				<p class="site-description"><?php echo $atelierdetimogo_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
+			<?php endif; ?>
+		</div><!-- .site-branding -->
+
+		<nav id="site-navigation" class="main-navigation">
+			<?php
+	wp_nav_menu( array( 
+	 'theme_location' => 'menu-1',
+	 'container' => 'ul' ) );
+			?>
+		</nav><!-- #site-navigation -->
+	</header><!-- #masthead -->
